bc289f12b34f09e490a0510206d19d85848bc2d9
Dom B <87432624+KnightBlue14@users.noreply.github.com>
2023-11-21 12:24:18 +0000

    Update plex_auth.py

--- plex_auth.py
+++ plex_auth.py
@@ -1,6 +1,6 @@
 baseurl = '{insert localhost url}:32400'
 token = '{Plex token}'
-host='SQLDock' #(Or whichever name you'd prefer)
+host='SQLDock' #(Or whichever name you'd prefer - make sure to update the compose file)
 user='root'
 passwd='{Insert password)'
 port=3306 #Set by default for MySQL
